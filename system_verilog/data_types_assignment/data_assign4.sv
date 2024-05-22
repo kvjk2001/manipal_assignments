438d3222013f1097d2ccd0d56b20a398029c9625
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-05-22 12:23:10 +0530

    Update data_assign4.sv

--- system_verilog/data_types_assignment/data_assign4.sv
+++ system_verilog/data_types_assignment/data_assign4.sv
@@ -1,7 +1,7 @@
 /************************************************************************
 Author: Mirafra Technologies Pvt Limited
         By Priya Ananthakrishnan
-Filename:	data_assign5.sv  
+Filename:	data_assign4.sv  
 Date:   	21st May 2022
 Version:	1.0
 Description: Concept of structure data type 
@@ -9,7 +9,7 @@ Description: Concept of structure data type
 //ADD_CODE: Declare the struct with name money
 //ADD_CODE: Store coins as type int and rupees as real
 
-module data_assign5;
+module data_assign4;
  //ADD_CODE: To create the instance of the structure
   initial begin
     //ADD_CODE: to Assign directvalues to the structure variables
