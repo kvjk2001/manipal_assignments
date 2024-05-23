ee502452b085d5d2a915e9bc7d1939495869a73c
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-05-23 15:55:42 +0530

    Update data_assign4.sv

--- system_verilog/data_types_assignment/data_assign4.sv
+++ system_verilog/data_types_assignment/data_assign4.sv
@@ -6,19 +6,20 @@ Date:   	21st May 2022
 Version:	1.0
 Description: Concept of structure data type 
 ***************************************************************************/
-//ADD_CODE: Declare the struct with name money
-//ADD_CODE: Store coins as type int and rupees as real
+//Declare the struct with name money as typedef
+//Store coins as type int and rupees as real
 
 module data_assign4;
- //ADD_CODE: To create the instance of the structure
-  initial begin
-    //ADD_CODE: to Assign directvalues to the structure variables
-    //Display the values of the memebers
+ //To create the instance of the structure
+  initial 
+    begin
+      //Assign directvalues to the structure variables
+      //Display the values of the memebers
     
-    //ADD_CODE: Assign values using the memebers name
-    //ADD_CODE: Display the values 
+      //Assign values using the memebers name
+      //Display the values 
     
-    //ADD_CODE: Assig all elements of structure to zero
-    //ADD_CODE: Diaplay the values 
+      //Assign all elements of structure to zero
+      //Display the values 
   end
 endmodule
