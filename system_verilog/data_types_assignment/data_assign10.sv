aef6e3a97dd23d7048c5e27e54b55456c63f9cff
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-05-23 15:59:31 +0530

    Update data_assign10.sv

--- system_verilog/data_types_assignment/data_assign10.sv
+++ system_verilog/data_types_assignment/data_assign10.sv
@@ -11,15 +11,15 @@ module data_assign10 ();
   // Example: Split the dyn_arr_b[0]=ffffffff  to 
   // dyn_arr_a[0]=ff, dyn_arr_a[1]=ff , dyn_arr_a[2]=ff, dyn_arr_a[3]=ff.
   
-  //ADD_CODE: Declare a packed array of size 32 bit named dyn_arr_b
-  //ADD_CODE: Declare a multidimensional array dyn_arr_a as specified in the example 
+  //Declare a packed array of size 32 bit named dyn_arr_b
+  //Declare a multidimensional array dyn_arr_a as specified in the example 
 
   initial 
     begin
-      //ADD_CODE: create and randomize the dyn_arr_b ;
-      //ADD_CODE: create and assign size for the dyn_arr_a
-      //ADD_CODE: Write the logic (loops) to split the dyn_arr_b and assign the values to dyn_arr_a
-      //ADD_CODE: Display both the arrays 
+      //create and randomize the dyn_arr_b ;
+      //create and assign size for the dyn_arr_a
+      //Write the logic (loops) to split the dyn_arr_b and assign the values to dyn_arr_a
+      //Display both the arrays 
     
   end
 endmodule
