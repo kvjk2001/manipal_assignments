3b3c81102ea22673b48cc8d7b3e5da20543cd33f
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-05-23 22:55:25 +0530

    Update data_assign2.sv
    Added the feedbacks from Alok

--- system_verilog/data_types_assignment/data_assign2.sv
+++ system_verilog/data_types_assignment/data_assign2.sv
@@ -12,22 +12,26 @@ module data_assign2;
   //Part 1
   //ADD_CODE: Declare a function and input the size 
   //ADD_CODE: Inside the function declare and create the array
-  //ADD_CODE: Use the appropriate loop to randomize the elemants and display them
+  //ADD_CODE: Use the appropriate loop to randomize the elemants of the array
+
 
   //Part 2a
   //ADD_CODE: The array method to return the maximum value stored in the array
-  //ADD_CODE: Display the array and the maximum value 
+
 
   //part 2b
   //ADD_CODE: Modify the Part 1 function to arrange the array with out array method
-  //ADD_CODE: Display the maximum value of the array and the array 
-
-  //Part 3
-  //ADD_CODE: Display the second highest value 
+  //ADD_CODE: write a function to arrange the the array in the ascending order
+  //HINT : using a loop you can compare the values of the array with previous value
   
+
   initial 
     begin
-    repeat(30)
-    //ADD_CODE: call the function
-  end
+      repeat(30)
+      //ADD_CODE: call the function
+      //ADD_CODE: Display the elements of the dynamic array
+      //ADD_CODE: Display the array and the maximum value 
+      //ADD_CODE: Display the maximum value of the array and second largest value in the array
+  
+    end
 endmodule
