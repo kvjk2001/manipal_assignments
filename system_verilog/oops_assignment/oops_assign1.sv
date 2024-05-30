ce2b238e60cf85f08122819432fde79ce8aa5c0b
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-05-30 20:36:17 +0530

    Update oops_assign1.sv
    Updated as per Aloks review comments

--- system_verilog/oops_assignment/oops_assign1.sv
+++ system_verilog/oops_assignment/oops_assign1.sv
@@ -9,14 +9,15 @@ Description: Concept of class constructor function and creation of Objects
 class construct
   //ADD_CODE: Declare a variable "i" of type int 
   //ADD_CODE: Decalre a class constructor function
-  //ADD_CODE: Increment the value of "i" by 1 inside the function
 endclass:construct
 
 module class_cons;
   //ADD_CODE: declare the three handles c0,c1,c2 for the calss "construct"
 initial
-    begin
-      //ADD_CODE: Create the Object for each handlle
-      //ADD_CODE: Display the values of i for each object by using the class handle
-  end
+  begin
+    //ADD_CODE: Create the Object for each handlle
+    //ADD_CODE: Display the values of i for each object by using the class handle
+    //ADD_CODE: Increment the value of "i" by 2  
+    //ADD_CODE: Display the values of i for each object by using the class handle      
+  end 
 endmodule:class_cons
