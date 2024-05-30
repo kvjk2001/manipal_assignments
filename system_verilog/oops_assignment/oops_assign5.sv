2522702243a42b64a46a684d3b5a2da8686fe012
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-05-30 12:06:26 +0530

    Create oops_assign5.sv

---
+++ system_verilog/oops_assignment/oops_assign5.sv
@@ -0,0 +1,8 @@
+/************************************************************************
+Author: Mirafra Technologies Pvt Limited
+        By Priya Ananthakrishnan
+Filename:	oops_assign5.sv  
+Date:   	30th May 2024
+Version:	1.0
+Description: Write a code for an example of Polymorphism
+***************************************************************************/
