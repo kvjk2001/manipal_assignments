dace84ec95463a946ff62f7f4347e8b4e9a0109d
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-05-30 13:35:51 +0530

    Update oops_assign10.sv

--- system_verilog/oops_assignment/oops_assign10.sv
+++ system_verilog/oops_assignment/oops_assign10.sv
@@ -1,7 +1,7 @@
 /************************************************************************
 Author: Mirafra Technologies Pvt Limited
         By Priya Ananthakrishnan
-Filename:	oops_assign7.sv  
+Filename:	oops_assign10.sv  
 Date:   	30th May 2024
 Version:	1.0
 Description: Concept for virtual class functionality
