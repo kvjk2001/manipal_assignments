343b639b025a2fedd70e04984ad5e03a72e58872
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2024-05-31 15:00:40 +0530

    Update data_assign1.sv

--- system_verilog/data_types_assignment/Solutions/data_assign1.sv
+++ system_verilog/data_types_assignment/Solutions/data_assign1.sv
@@ -1,3 +1,5 @@
+// Code your testbench here
+// or browse Examples
 function void display(int q_int_1[$]);
    $display("Size of unique queue: %0d, Unique queue: %p", q_int_1.size, q_int_1);
 endfunction
@@ -5,12 +7,16 @@ module data_assign1 (); // module name and file name same
   
 // Declared an associative array packet of type int and index type string
 int  packet[string];
+  int num;
   
 //Declared a unbounded queue of type int 
   int q_int[$]={500,1000,500,200,400,500,600,700,900,200,0};    
 //Assigned values to the queue elements 
   int packet_1[];
   int q_int_1[$];
+  string result[$];
+   int queue_1[$];
+  int result_queue[$];
 initial 
   begin
     // Assigned values to the array elements
@@ -24,12 +30,19 @@ initial
     // Display the size and values of all the array elements
     $display("Size of array: %0d,array: %p", packet.size, packet);
     // Add the array functions or methods to find the repeated values:
+    
     // Delete the duplicate elements from the array
     // Display the size and value of the modified  array size in an empty function
     // Expected output is size is 4
     // packet  = `{32,4,31,2};
-    packet_1 = packet.unique;
-    $display("Size of unique array: %0d, Unique array: %p", packet_1.size, packet_1);
+    foreach(packet[i])
+      begin
+        result = packet.find_index(num) with (num ==packet[i]);
+        if(result.size()>1)
+          packet.delete(result[0]);
+      end
+    $display("Packet after deleting the repeated values :  %p",packet);
+ 
     //Assigned values to the queue elements 
     q_int = {500,1000,500,200,400,500,600,700,900,200,0};        
 
@@ -45,8 +58,14 @@ initial
     //Display the size and value of the modified queue in an empty function
     //Expected output is size is 8
     // q_int = `{500,1000,200,400,600,700,900,0}
-	q_int_1 = q_int.unique;
-    display(q_int_1);
+    foreach(q_int[i])
+      begin
+        result_queue = q_int.find_index(num) with (num ==q_int[i]);
+        if(result_queue.size()>1)
+          q_int.delete(result_queue[0]);
+      end
+    $display("Queue after deleting the repeated values :  %p",q_int);
+
     #10 $finish;
   end
 endmodule
