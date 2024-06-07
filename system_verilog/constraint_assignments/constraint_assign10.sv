08fe57a93b6fbc3226b083718e5309a8d5ed2033
Meenal <114145036+meenalpannase@users.noreply.github.com>
2024-06-07 20:45:54 +0530

    Update constraint_assign10.sv

--- system_verilog/constraint_assignments/constraint_assign10.sv
+++ system_verilog/constraint_assignments/constraint_assign10.sv
@@ -1,1 +1,14 @@
+/************************************************************************
+Author: Mirafra Technologies Pvt Limited
+        By Priya Ananthakrishnan
+Filename:	func_assign10.sv  
+Date:   	7th June 2024
+Version:	1.0
+Description: Concept of Constraint Randomization in System Verilog 
+***************************************************************************/
+//What is wrong with the below code? What is the correct process to write the constraint?
+Class const;
+    rand bit [7:0] low, mid, high;
+    constraint Const_1 {low <mid<high;}
+endclass
 
