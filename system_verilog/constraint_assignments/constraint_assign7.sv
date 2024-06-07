8afa59940ed1c0a3c4af6801644b012f1fa086a1
Meenal <114145036+meenalpannase@users.noreply.github.com>
2024-06-07 20:36:11 +0530

    Update constraint_assign7.sv

--- system_verilog/constraint_assignments/constraint_assign7.sv
+++ system_verilog/constraint_assignments/constraint_assign7.sv
@@ -1,24 +1,24 @@
 /************************************************************************
 Author: Mirafra Technologies Pvt Limited
         By Priya Ananthakrishnan
-Filename:	func_assign6.sv  
+Filename:	func_assign7.sv  
 Date:   	7th June 2024
 Version:	1.0
 Description: Concept of Constraint Randomization in System Verilog 
 ***************************************************************************/
-class constraint_assign6;
+class constraint_assign7;
   
-//ADD_CODE: Declare the dynamic array of mem_address. 
-//ADD_CODE: Write for constraint to generate a dynamic array mem_address of size 128 to 256 elements. Each element has a difference of 4 with respect to neighbour element. 
+//ADD_CODE: Declare the 32bit variable. 
+//ADD_CODE: Write for constraint to Generate a random 32-bit value With alternate pattern {0,1,0,1,...}  
   
-endclass:constraint_assign6
-module con_assin6; 
-  //ADD_CODE: Declare the handle for "class constraint_assign6" as c0.
+endclass:constraint_assign7
+module con_assin7; 
+  //ADD_CODE: Declare the handle for "class constraint_assign7" as c0.
 initial
   begin
     //ADD_CODE: Create the Object for  handle
-    //ADD_CODE: Randomize the object for genrating randomize values for dymanic array of size between range of 128to 256 with difference of with respect to neighnour element.
-    //ADD_CODE: Display the values and size of dynamic array using object handel.
+          //ADD_CODE: Randomize the object for genrating randomize values of variable with pattern {0,1,0,1..}.
+    //ADD_CODE: Display the values of variable using object handel.
         
   end 
-endmodule:con_assign6
+endmodule:con_assign7
