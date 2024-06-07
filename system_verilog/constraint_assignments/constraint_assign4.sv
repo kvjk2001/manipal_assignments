c9784ef85a8f5db474ba9f40cd13e44c88e2f399
Meenal <114145036+meenalpannase@users.noreply.github.com>
2024-06-07 20:02:10 +0530

    Update constraint_assign4.sv

--- system_verilog/constraint_assignments/constraint_assign4.sv
+++ system_verilog/constraint_assignments/constraint_assign4.sv
@@ -1,1 +1,24 @@
-
+/************************************************************************
+Author: Mirafra Technologies Pvt Limited
+        By Priya Ananthakrishnan
+Filename:	func_assign4.sv  
+Date:   	7th June 2024
+Version:	1.0
+Description: Concept of Constraint Randomization in System Verilog 
+***************************************************************************/
+class constraint_assign4;
+  
+//ADD_CODE: Declare the 8bit variable as data. 
+//ADD_CODE: Write constraint for dynamic array of randomize size between 10 to 20 to generate odd numbers.
+  
+endclass:constraint_assign4
+module con_assin4; 
+  //ADD_CODE: Declare the handle for "class constraint_assign4" as c0.
+initial
+  begin
+    //ADD_CODE: Create the Object for  handle
+    //ADD_CODE: Randomize the object for genrating randomize values for array of size between 10 to 20 with odd numbers.
+    //ADD_CODE: Display the values and size of array using object handel.
+        
+  end 
+endmodule:con_assign4
