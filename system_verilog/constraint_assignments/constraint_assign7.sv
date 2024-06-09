cf68a944eea540da8672dc4ab385be30295061d8
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-06-09 14:00:19 -0500

    Update constraint_assign7.sv

--- system_verilog/constraint_assignments/constraint_assign7.sv
+++ system_verilog/constraint_assignments/constraint_assign7.sv
@@ -1,7 +1,7 @@
 /************************************************************************
 Author: Mirafra Technologies Pvt Limited
-        By Priya Ananthakrishnan
-Filename:	func_assign7.sv  
+        By Meenal Sitaram Pannase
+Filename:	constraint_assign7.sv  
 Date:   	7th June 2024
 Version:	1.0
 Description: Concept of Constraint Randomization in System Verilog 
@@ -10,14 +10,14 @@ class constraint_assign7;
   
 //ADD_CODE: Declare the 32bit variable. 
 //ADD_CODE: Write for constraint to Generate a random 32-bit value With alternate pattern {0,1,0,1,...}  
-  
+        //a = 0101010101 (32 bits)
 endclass:constraint_assign7
-module con_assin7; 
-  //ADD_CODE: Declare the handle for "class constraint_assign7" as c0.
+module con_assign7; 
+//ADD_CODE: Declare the handle for "class constraint_assign7" as c0.
 initial
   begin
     //ADD_CODE: Create the Object for  handle
-          //ADD_CODE: Randomize the object for genrating randomize values of variable with pattern {0,1,0,1..}.
+    //ADD_CODE: Randomize the object for genrating randomize values of variable with pattern {0,1,0,1..}.
     //ADD_CODE: Display the values of variable using object handel.
         
   end 
