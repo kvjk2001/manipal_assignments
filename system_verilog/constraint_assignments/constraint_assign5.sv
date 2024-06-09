841a38253c5152be7e817ce7bde92b3b4ae08e5e
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-06-09 13:36:04 -0500

    Update constraint_assign5.sv

--- system_verilog/constraint_assignments/constraint_assign5.sv
+++ system_verilog/constraint_assignments/constraint_assign5.sv
@@ -1,7 +1,7 @@
 /************************************************************************
 Author: Mirafra Technologies Pvt Limited
-        By Priya Ananthakrishnan
-Filename:	func_assign5.sv  
+        By Meenal Sitaram pannase
+Filename:	constraint_assign5.sv  
 Date:   	7th June 2024
 Version:	1.0
 Description: Concept of Constraint Randomization in System Verilog 
@@ -11,17 +11,17 @@ class constraint_assign5;
 //ADD_CODE: Declare the 8bit variable as data. 
 //ADD_CODE: Write constraint  for 8 bit variable b which will generate a values between range of 100to 200.
 //ADD_CODE: Declare the 8bit variable as data1. 
-//ADD_CODE: Write constraint  for 8 bit variable b which will generate a values between range of
+//ADD_CODE: Write constraint  for 8 bit variable b_unique which will generate a values between range of
 //100 to 200 without using unique keyword . 
 
 endclass:constraint_assign5
-module con_assin5; 
+module con_assign5; 
   //ADD_CODE: Declare the handle for "class constraint_assign5" as c0.
 initial
   begin
     //ADD_CODE: Create the Object for  handle
-    //ADD_CODE: Randomize the object for genrating randomize values for variable b between range of 100to 200..
-    //ADD_CODE: Display the values of variabkle b using object handel.
+    //ADD_CODE: Randomize the object for generating randomize values for variable b and b_unique between range of 100to 200..
+    //ADD_CODE: Display the values of variable b using object handel.
         
   end 
 endmodule:con_assign5
