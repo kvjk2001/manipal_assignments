3ebcf067815103346a25f7bf544b8053861d7c88
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-06-09 13:20:42 -0500

    Update constraint_assign3.sv

--- system_verilog/constraint_assignments/constraint_assign3.sv
+++ system_verilog/constraint_assignments/constraint_assign3.sv
@@ -1,6 +1,6 @@
 /************************************************************************
 Author: Mirafra Technologies Pvt Limited
-        By Priya Ananthakrishnan
+        By Meenal Sitaram Pannase
 Filename:	func_assign3.sv  
 Date:   	7th June 2024
 Version:	1.0
@@ -12,7 +12,7 @@ class constraint_assign3;
 //ADD_CODE: Write constraint the random variable [31:0]a such that the randomized values are always multiples of 10.
   
 endclass:constraint_assign3 
-module con_assin3; 
+module con_assign3; 
   //ADD_CODE: Declare the handle for "class constraint_assign3" as c0.
 initial
   begin
