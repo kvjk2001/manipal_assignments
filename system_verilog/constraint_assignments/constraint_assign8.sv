b16781da1e5b6166fb936357ceaf1dd62b103a48
apriya-ram <61039744+apriya-ram@users.noreply.github.com>
2024-06-09 14:08:20 -0500

    Update constraint_assign8.sv

--- system_verilog/constraint_assignments/constraint_assign8.sv
+++ system_verilog/constraint_assignments/constraint_assign8.sv
@@ -1,7 +1,7 @@
 /************************************************************************
 Author: Mirafra Technologies Pvt Limited
-        By Priya Ananthakrishnan
-Filename:	func_assign8.sv  
+        By Meenal Sitram Pannase
+Filename:	constraint_assign8.sv  
 Date:   	7th June 2024
 Version:	1.0
 Description: Concept of Constraint Randomization in System Verilog 
@@ -12,12 +12,12 @@ class constraint_assign8;
 //ADD_CODE: Write for constraint for two variable of 32bit a and b which will generate the random values for a greater that 150 and for b greater than 150 less than 1000.
   
 endclass:constraint_assign8
-module con_assin8; 
+module con_assign8; 
   //ADD_CODE: Declare the handle for "class constraint_assign8" as c0.
 initial
   begin
     //ADD_CODE: Create the Object for  handle
-          //ADD_CODE: Randomize the object for genrating randomize values of variable a and b.
+    //ADD_CODE: Randomize the object for genrating randomize values of variable a and b.
     //ADD_CODE: Display the values of variable a and b using object handel.
         
   end 
