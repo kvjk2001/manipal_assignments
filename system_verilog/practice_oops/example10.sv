589a719f4159b61625a00f56690947bc01f985f1
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2024-06-12 09:47:52 +0530

    Update example10.sv

--- system_verilog/practice_oops/example10.sv
+++ system_verilog/practice_oops/example10.sv
@@ -11,16 +11,16 @@ typedef bit [7:0] arg1_t;
 typedef logic [7:0] arg2_t;
 program example15;
  class base1;
- virtual function bit showit (arg1_t i1);
- $display ("\n\n %m: i1=%0d \n\n", i1);
- endfunction
+   virtual function bit showit (arg1_t i1);
+     $display ("\n\n %m: i1=%0d \n\n", i1);
+   endfunction
  endclass
 
  class ext1 extends base1;
- virtual function bit showit (arg2_t i1);
- $display ("\n\n EXT: %m: i1=%0d \n\n", i1);
- return(0);
- endfunction
+   virtual function bit showit (arg2_t i1);
+      $display ("\n\n EXT: %m: i1=%0d \n\n", i1);
+      return(0);
+   endfunction
  endclass
 
  base1 b1 = new();
@@ -29,7 +29,7 @@ program example15;
  bit status;
 
  initial begin
- status=b1.showit(25);
- status=e1.showit(33);
+   status=b1.showit(25);
+   status=e1.showit(33);
  end
 endprogram
