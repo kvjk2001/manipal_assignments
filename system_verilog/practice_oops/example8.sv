d3eb0b079d44be977f9ae1df069c7ed7a1da6ee1
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2024-06-12 09:44:46 +0530

    Update example8.sv

--- system_verilog/practice_oops/example8.sv
+++ system_verilog/practice_oops/example8.sv
@@ -8,16 +8,16 @@ Description: Concept of virtual polymorphism
 ***************************************************************************/
 program example8;
  class base1;
- virtual function bit showit (arg1_t i1);
- $display ("\n\n %m: i1=%0d \n\n", i1);
- endfunction
+   virtual function bit showit (arg1_t i1);
+     $display ("\n\n %m: i1=%0d \n\n", i1);
+   endfunction
  endclass
 
  class ext1 extends base1;
- virtual function bit showit (arg1_t i1);
- $display ("\n\n EXT: %m: i1=%0d \n\n", i1);
- return(1);
- endfunction
+   virtual function bit showit (arg1_t i1);
+     $display ("\n\n EXT: %m: i1=%0d \n\n", i1);
+     return(1);
+   endfunction
  endclass
 
  base1 b1 = new();
@@ -26,7 +26,7 @@ program example8;
  bit status;
 
  initial begin
- status=b1.showit(25);
- status=e1.showit(33);
+   status=b1.showit(25);
+   status=e1.showit(33);
  end
 endprogram 
