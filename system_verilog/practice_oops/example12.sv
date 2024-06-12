e92c0515209db4358f8e7d2e83dedd3a92cf9ff3
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2024-06-12 09:45:29 +0530

    Update example12.sv

--- system_verilog/practice_oops/example12.sv
+++ system_verilog/practice_oops/example12.sv
@@ -17,29 +17,29 @@ program example12();
     pure virtual function void print_id();
   endclass
  // First extension; add field and pure virtual override.
- class ext1 extends base1;
- bit [3:0] e1_type;
-   function new(int sid, int se1);
-     super.new(sid);
-     e1_type = se1;
-   endfunction
-   virtual function void print_id();
+  class ext1 extends base1;
+    bit [3:0] e1_type;
+    function new(int sid, int se1);
+      super.new(sid);
+      e1_type = se1;
+    endfunction
+    virtual function void print_id();
      $display("ID %0d: E1 %0d\n", id, e1_type);
-   endfunction
- endclass
+    endfunction
+  endclass
  // Second extension; pure virtual override.
- class ext2 extends base1;
+  class ext2 extends base1;
    function new(int sid);
      super.new(sid);
    endfunction 
    virtual function void print_id();
      $display("ID %0d: E2\n", id);
    endfunction
- endclass
- base1 base;
- ext1 e1 = new(0, 3);
- ext2 e2 = new(1);
- initial 
+  endclass
+  base1 base;
+  ext1 e1 = new(0, 3);
+  ext2 e2 = new(1);
+  initial 
    begin
      base = e1;
      base.print_id();
